e7abbfb56cf878a00e676cee21bb15f2789d1786
minjko <komj36@gmail.com>
2022-10-29 10:19:37 +0900

    create project Banabada

---
+++ src/main/java/com/example/banabada/BanabadaApplication.java
@@ -0,0 +1,13 @@
+package com.example.banabada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanabadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanabadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/banabada/BanabadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.banabada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanabadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
