fe6aa663e03d73ab950fec83c8f3056d8c179bdb
gudchd <mon101soon@gmail.com>
2022-11-08 19:33:49 +0900

    상품목록페이지 1...

--- front/banabaDa/src/pages/products/index.tsx
+++ front/banabaDa/src/pages/products/index.tsx
@@ -1,3 +1,25 @@
-const ProductList = () => <div>목록페이지</div>
+import { useQuery } from "react-query"
+import ProductItem from "../../components/product/item"
+import {fetcher, QueryKeys } from "../../queryClient"
+import { Product } from "../../types"
+
+const ProductList = () => {
+    const { data } = useQuery<Product[]>(QueryKeys.PRODUCTS, () => fetcher({
+        method: 'GET',
+        path: '/products'
+    }),
+    )
+
+    return (
+        <div>
+            <ul>
+                {data?.map(product=> ( //백엔드와 데이터명 맞춰서 수정할 것
+                    <ProductItem {... product} key={product.id}/>
+                ))}
+            </ul>
+        </div>
+    )
+
+}
 
 export default ProductList
