074dfcfd254338c6f3e1fd4882ab8183f75d87fe
minjko <komj36@gmail.com>
2022-11-14 21:13:15 +0900

    충돌 후 다시 올림

--- src/main/java/com/example/banabada/model/CartEntity.java
+++ src/main/java/com/example/banabada/model/CartEntity.java
@@ -5,8 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,14 +27,19 @@ public class CartEntity {
 
     // 외래 키
     // 사용자 ID
-    @OneToOne
-    @JoinColumn
-    private UserEntity user;
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn //entity 내부에서 joincolum 이름이 같으면 안됨
+    private UserEntity user;  // 구매자
     // 스프링 어노테이션 @AuthenticationPrincipal 사용하여 세션정보 넘길 수 있음
     // private String userId;로 변경 고민 중  --> OneToOne도 가능한지 봐야 함
+    // 카트에 담긴 총 상품 개수
+    private int count;
     // 장바구니 상품 ID
     @OneToMany(mappedBy="cart")
     private List<CartItemEntity> cartItemList = new ArrayList<CartItemEntity>();
+
+    @DateTimeFormat(pattern = "yyyy-mm-dd")
+    private LocalDate createDate; // 날짜(장바구니 담은 날짜, 필요한 이유는 ㅁㄹ)
 
     private boolean yes; // true : 기본적으로 선택하는 걸로 결정!
 
--- src/main/java/com/example/banabada/model/CartItemEntity.java
+++ src/main/java/com/example/banabada/model/CartItemEntity.java
@@ -25,12 +25,28 @@ public class CartItemEntity {
 
     // 외래 키
     // 장바구니 ID
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn
     private CartEntity cart;
     // 상품 ID
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn
     private ProductEntity product;
+
+    // 상품 개수
+    private int count;
+
+    public static CartItemEntity createCartItem(CartEntity cart, ProductEntity product, int amount) {
+        CartItemEntity cartItem = new CartItemEntity();
+        cartItem.setCart(cart);
+        cartItem.setProduct(product);
+        cartItem.setCount(amount);
+        return cartItem;
+    }
+
+    // 이미 담겨있는 물건 또 담을 경우 수량을 증가시키기
+    public void addCount(int count) {
+        this.count += count;
+    }
 
 }
